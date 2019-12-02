99aa3d827e5d1274fa94b30ce0c7a464bc39b081
Ubuntu <ubuntu@ip-172-31-24-184.us-east-2.compute.internal>
2019-12-02 16:00:26 +0000

    submitting assessment

--- js-foundations.js
+++ js-foundations.js
@@ -2,11 +2,42 @@
 // Coding practical questions
 
 // 1. Write a function that takes a number as an argument and decides if the number is evenly divisble by three or not. Use the test cases 15, 0, 385, -7. Expected output: "15 is divisible by three", "0 is divisible by three", "385 is not divisble by three", "-7 is not divisble by three".
+// Write a function called divisbleBy
+// Take in an argument of a number, 15, 0, 385, -7
+// Uses an if/else statement to check if %3 === 0
+// Returns the correct statement whether or not it's evenly divisible by 
 
+var numOne = 15
+var numTwo = 0
+var numThree = 385
+var numFour = -7
 
+const divisbleBy = (number) => {
+    if (number%3 === 0){
+        return `${number} is evenly divisible by 3.`
+    } else {
+        return `${number} is not evenly divisible by 3.`
+    }
+}
+console.log(divisbleBy(numOne))
+console.log(divisbleBy(numTwo))
+console.log(divisbleBy(numThree))
+console.log(divisbleBy(numFour))
 
 // 2. Create an object called helloMe. Include your firstName, lastName and TWO OTHER properties of you. Add a method to your object that will return a sentence using the object properties to describe you.
-
+// object called helloMe
+// the object will include firstName, lastName, age, numSiblings
+// Use a method to return a sentence using the properties
+var person = {
+    firstName: "Kristen",
+    lastName: "Mulder",
+    age: 27,
+    numSiblings: 2,
+    getData: function(){
+        return "Hi, my name is " + this.firstName +" "+ this.lastName + ". I am " + this.age + " years old and I have "+this.numSiblings+ " siblings." 
+    }
+}
+console.log(person.getData())
 
 
 // ------------------------------------- Consider the variable:
@@ -14,37 +45,112 @@
 var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
 
 // 3a. Write a function that returns every other item from the array as one string. Expected output: "streetlamp teeth nephew database"
+// write a function calle everyOther 
+// Takes in an array as an argument 
+// Filters every other index by using indexOf formula to check %2 and returns a new array
+// Joins the new array into a string
 
+const everyOther = (array) => {
+    return array.filter(value => {
+        return array.indexOf(value) %2 === 0
+    }).join(" ")
+}
+console.log(everyOther(randomNouns))
 
 
 // 3b. Create a function that takes in the variable and returns the array with all the words capitalized. Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
+// write a function called firstLetterCapitalized
+// Takes an array as an argument
+// Maps through the array and changes the first string index to a capital letter
+// returns the array 
 
-
+const firstLetterCapitalized = (array) => {
+    return array.map(value => {
+        return value[0].toUpperCase() + value.slice(1)
+    })
+}
+console.log(firstLetterCapitalized(randomNouns))
 
 // 3c. STRETCH: Create a function that returns the length of each string from the randomNouns variable. Expected output: [10, 6, 5, 10, 6, 11, 8]
+// A function called stringLength
+// Takes an array as an argument
+// Maps through the array and returns the length of each value in the array
+// returns new array with the length of the string
 
-
-
+const stringLength = (array) => {
+    return array.map(value => {
+        return value.length
+    })
+}
+console.log(stringLength(randomNouns))
 
 // -------------------------------------- Consider the variables:
 var testString1 = "learn"
 var testString2 = "academy"
 var testString3 = "sandiego"
 
+
 // 4a. Write a function called alphabetSoup that takes one argument and returns a string with all the letters in alphabetical order. Use each of the varibales as test cases. Expected output: "aelnr" "aacdemy" "adeginos"
+// Function called alphabetSoup
+// Takes one argument of a string 
+// Splits the string into an array
+// Sorts the array into alphabetical order
+// Returns the new array joined as a new string 
 
-
+const alphabetSoup = (string) =>{
+    let newArray = string.split("")
+    return (newArray.sort()).join("")
+}
+console.log(alphabetSoup(testString1))
+console.log(alphabetSoup(testString2))
+console.log(alphabetSoup(testString3))
 
 // 4b. Write a function that takes in all three variables and returns a string with all the letters in alphabetical order. Expected output: "aaaacddeeegilmnnorsy"
+// function called alphabetStew
+// Takes three arguments of strings
+// Joins the string together into one string and then splits into an array
+// Sorts the array into alphabetical order
+// Returns the new array joined as a new string 
 
-
+const alphabetStew = (string1, string2, string3) =>{
+    let newString = string1.concat(string2, string3)
+    let newArray = newString.split("")
+    return (newArray.sort()).join("")
+}
+console.log(alphabetStew(testString1, testString2, testString3))
 
 // ------------------------------------- Consider the variables:
 var amounts = [9, 1, 8, 16, 5, 1, 42]
 var animals = ["ducks", "elephants", "pangolins", "zebras", "giraffes", "penguins", "llamas"]
 
 // 5a. Write a function that takes the two variables and returns a combined string. Expected output: "9 ducks 1 elephants 8 pangolins 16 zebras 5 giraffes 1 penguins 42 llamas"
+// Write a function called combinedString
+// Takes two arguments of arrays
+// Maps through one array and returns the equivalent element in the corresponding index of the other array
+// Returns the array joined as a string
 
-
+const combinedString = (arr1, arr2) => {
+    return arr1.map((v,i) => {
+        return [v + " " + arr2[i]]
+    }).join(" ")
+}
+console.log(combinedString(amounts, animals))
 
 // 5b. STRETCH: Create a function that combines the two variables and updates the animal to be singular if the animal's corresponding number is 1. Expected output: "9 ducks 1 elephant 8 pangolins 16 zebras 5 giraffes 1 penguin 42 llamas"
+// Write a function called combinedStringTwo
+// Takes two arguments of arrays
+// Maps through one array and runs an if/ else statement to check if the element in the first array has a value of 1. 
+// If it does, it returns the equivalent element in the corresponding index of the other array without an 's'
+// If it doesn't, returns the equivalent element in the corresponding index of the other array
+// Function will return the array joined as a string
+
+const combinedStringTwo = (arr1, arr2) => {
+    return arr1.map((v,i) => {
+        if (v === 1) {
+            return [v + " " + arr2[i].slice(0,(arr2[i].length - 1))]
+        } else {
+            return [v + " " + arr2[i]]
+        }
+    }).join(" ")
+}
+console.log(combinedStringTwo(amounts, animals))
--- js-foundations.md
+++ js-foundations.md
@@ -14,67 +14,75 @@ logIt()
 
 
 1a. Look at this Javascript function and try to predict what the console show.
-
+The console that is inside the function will log 'outside' because it occurs before we re-assign the variable since Javascript reads line by line. If we console log the function, it should then display 'inside.' 
 
 
 1b. Test the function. Explain why the function returned what it did.
 
-  Your answer:
+  Your answer: When I console the function with test as the argument, I get 'undefined' twice. 
 
-  Researched answer:
+  Researched answer: When I built the function myself, I could see several issues with the above function, here's how I would write it: 
+  var text2 = "outside" // proper string notation was missing
+const logIt2 = () => { 
+    console.log(text2)
+    return text = "inside" // the return statement was missing and var text was already assigned outside of the function so we don't need to write "var" again.
+}
+console.log(logIt2(text2)) //This needed a console log with an argument
 
+  
 
 2. What is JSON?
 
-  Your answer:
+  Your answer: Javascript code
 
-  Researched answer:
+  Researched answer: JSON stands for JavaScript Object Notation. JSON allows us to send a JavaScript object to the server so that we can work with Javascript objects. 
 
 
 3. What does CRUD stand for?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
+  Researched answer: Create, Read, Update, Delete - I did not know this one!
 
 
 
-4. What does are the 5 HTTP verbs?
+4. What are the 5 HTTP verbs?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
+  Researched answer: GET, POST, DELETE, PATCH, PUT (Have we used these yet? I don't recognize them, but I found an article that named them.)
 
 
 5. What is a higher-order function?
 
-  Your answer:
+  Your answer: Higher order functions act on arrays by iterationg through the array. .map will take a value and index and act on an array, returning the same array length. .filter will take a value but not return the same array length, since it's filtering out some of the values within your array. .reduce is a good method when you want to both .filter and .map. 
 
-  Researched answer:
+  Researched answer: A higher order function is a function that takes a function as an argument, or returns a function. .reduce takes an accumulator and the current value in the list and returns a new accumulated value. 
 
 
 6. STRETCH: What is a closure, what is it good for and how do you recognize one?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
+  Researched answer: A closure is the function and the scope of the variables within the enviornment of the function. 
 
 
 7. STRETCH: What is an API?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
+  Researched answer: Application Programming Interface, it allows the user to access data from a web application. 
 
 
 ### Additional Feedback Questions.
 
 1. Do you have a suggestion for a Check In question?
-
+If you could freaky Friday with a fictional character for one day, who it would be? Keep in mind, that means the character you choose would also be you for a day!
 
 
 2. What was your favorite topic this week?
-
+Practicing with array methods in the pig latin translator challenge
 
 
 3. What was your "A-ha!" moment this week?
+Console.log() helps you understand how your code is working, especially when passing arguments through it. This helps when your code is not returning what you expected, because you can see in the console.log where it's going wrong. 
